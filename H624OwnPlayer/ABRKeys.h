463e7eb7b3938ddf99bb628475d13c36200f00cb
Yazid <tyazid@gmail.com>
2017-04-01 02:06:58 +0200

    Update ABRKeys.h

--- H624OwnPlayer/ABRKeys.h
+++ H624OwnPlayer/ABRKeys.h
@@ -6,18 +6,9 @@
 //  Copyright © 2017 tyazid. All rights reserved.
 //
 
-#ifndef SvqKeys_h
-#define SvqKeys_h
-/** KEY USED TO CONFIGURE XPLAYER SMART-ABR FEATURE (using Xplayer#setExtraConfiguration)
- Exp:
- [[self player] setExtraConfiguration:
- [NSDictionary dictionaryWithObjectsAndKeys:
- [NSNumber numberWithBool:YES],SVQAN_SMART_AVTIVATE_KEY,
- [NSNumber numberWithFloat:8.0f], SVQAN_NOTE_THRESHOLD_KEY,
- [NSNumber numberWithFloat:0.3f],SVQAN_DELTA_NOTE_KEY,
- nil]] ;
- **/
-
+#ifndef ABRKeys_h
+#define ABRKeys_h
+ 
 
 //FLOAT FRAC 0]..1]
 #define BANDWIDTH_FRACTION @"abr.bandwidth.fraction"
@@ -27,4 +18,4 @@
 /**  NOTIFICATION CENTER FOR SMART ABR   **/
 #define SVQN_STAT_EVENT_NAME "abr.event.key"
 #define NOTIFICATION_SMARTABR_CENTER_NAME  @"XplayertNotification.smart.abr.notification"
-#endif /* SvqKeys_h */
+#endif /* ABRKeys_h */
